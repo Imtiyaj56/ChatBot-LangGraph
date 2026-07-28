9889e35c97e66e6544a2e28f209fd8affbad8473
Imtiyaj56 <imtiyajsunasara056@gmail.com>
2026-07-28 20:02:33 +0530

    LangSmith Integrated

--- streamlit_frontend_with_database.py
+++ streamlit_frontend_with_database.py
@@ -80,7 +80,12 @@ if user_input:
 
 
 
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']},
+              'metadata': {
+                  'thread_id': st.session_state['thread_id']
+              },
+              'run_name': 'chat_turn'
+              }
 
     with st.chat_message('AI'):
         ai_message = st.write_stream(
